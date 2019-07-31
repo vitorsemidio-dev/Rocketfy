3b46bca48c445141606955f255c85c0d6aec0ac7
Vitor Emidio <vitorsemidio.96@gmail.com>
2019-07-30 21:18:04 -0300

    Utilizando dados estáticos vindo do arquivo api.js

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 
 import { Container, Label } from './styles';
 
-export default function Card() {
+export default function Card({ data }) {
   return (
     <Container>
       <header>
-        <Label color="#7159c1" />
+        { data.labels.map(label => <Label key={label} color={label}/>) }
       </header>
-      <p>Fazer uma aplicacao para listar playlists</p>
-      <img src="https://api.adorable.io/avatars/136/abott@adorable.pngCo" alt="Imagem de Perfil"></img>
+      <p>{ data.content }</p>
+      { data.user && <img src={data.user} alt="Imagem de Perfil"></img> }
     </Container>
   );
 }
